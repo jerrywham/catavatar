ff87ae3e561f7ff692405c5dfd9ca20695468cd9
jerrywham <github@ecyseo.net>
2017-02-15 00:13:44 +0100

    Create fr.php

---
+++ lang/fr.php
@@ -0,0 +1,6 @@
+<?php 
+	$LANG = array(
+		"L_TITLE"=>"cat-avatar-generator",
+		"L_DESCRIPTION"=>"A generator of cats pictures optimised to generate or random avatars, or defined avatar from a \"seed\". A derivation by [David Revoy](http://www.peppercarrot.com) from the original [MonsterID by Andreas Gohr's](https://www.splitbrain.org/blog/2007-01/20_monsterid_as_gravatar_fallback)."
+	);
+?>
