772e0e5228ddcc91aaa5b4e7a1619c7a0f928aa3
jerrywham <github@ecyseo.net>
2017-02-16 02:22:53 +0100

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -1,11 +1,11 @@
 <?php 
 	$LANG = array(
-		'L_TITLE'=>'cat-avatar-generator',
-		'L_DESCRIPTION'=>'A generator of cats pictures optimised to generate or random avatars, or defined avatar from a "seed". A derivation by [David Revoy](http://www.peppercarrot.com) from the original [MonsterID by Andreas Gohr\'s](https://www.splitbrain.org/blog/2007-01/20_monsterid_as_gravatar_fallback).',
-        'L_CACHE_PATH' => 'Chemin du cache des images',
-        'L_STRING_FOR_ADMIN' => 'Chaîne permettant de personnaliser votre chavatar',
+        'L_TITLE'             =>'cat-avatar-generator',
+        'L_DESCRIPTION'       =>'A generator of cats pictures optimised to generate or random avatars, or defined avatar from a "seed". A derivation by [David Revoy](http://www.peppercarrot.com) from the original [MonsterID by Andreas Gohr\'s](https://www.splitbrain.org/blog/2007-01/20_monsterid_as_gravatar_fallback).',
+        'L_CACHE_PATH'        => 'Chemin du cache des images',
+        'L_STRING_FOR_ADMIN'  => 'Chaîne permettant de personnaliser votre chavatar',
         'L_HELP_ADMIN_STRING' => 'La chaîne peut être vide ou contenir 35 caractères au maximum. Les autres caractères ne seront pas pris en compte',
-        'L_YOUR_CATAVATAR' => 'Votre chatavatar',
+        'L_YOUR_CATAVATAR'    => 'Votre chatavatar',
 
 	);
 ?>
