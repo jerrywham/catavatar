69ed17324688c894fc3b1c3a96f0325edb95945e
jerrywham <github@ecyseo.net>
2017-08-29 16:37:30 +0200

    Create fr-help.php

---
+++ lang/fr-help.php
@@ -0,0 +1,2 @@
+<p>Pour afficher les chatavatars, ajoutez le code suivant dans le fichier commentaires.php de votre thème, où vous souhaitez.</p>
+<pre><code>&lt;?php echo $plxShow->plxMotor->plxRecord_coms->f('catavatar') ?&gt;</code></pre>
